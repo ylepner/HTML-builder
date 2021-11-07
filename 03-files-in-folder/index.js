1c680739a1ad22e42ce743a8529cc270c9ae3d80
Yulia Lepner <ylepner@gmail.com>
2021-11-07 17:31:38 +0100

    refactor: change name of var

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,11 +1,11 @@
 const path = require('path');
-let testFolder = path.join('03-files-in-folder', 'secret-folder')
+let folder = path.join('03-files-in-folder', 'secret-folder')
 const { readdir, lstat } = require('fs/promises');
 
 async function main() {
-  const files = await readdir(testFolder);
+  const files = await readdir(folder);
   for (const fileOrDir of files) {
-    const stat = await lstat(path.join(testFolder, fileOrDir));
+    const stat = await lstat(path.join(folder, fileOrDir));
     if (stat.isFile()) {
       const fileName = path.parse(fileOrDir)
       console.log(`${fileName.name} - ${fileName.ext.substring(1)} - ${stat.size / 1024} kb`);
